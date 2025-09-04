26ccdcf8dd0da9a5b28a9eda5d8878af17e4ca28
alirezahs.it <alirezahs.it@gmail.com>
2025-09-05 03:20:17 +0330

    'init'

--- serve.bat
+++ serve.bat
@@ -1,10 +1,11 @@
 @echo off
 chcp 65001 >nul
-echo Starting local web server with UTF-8 support...
+set PYTHONIOENCODING=utf-8
+echo Starting local web server with Persian/UTF-8 support...
 echo.
 echo The website will be available at: http://localhost:8000
 echo.
 echo Press Ctrl+C to stop the server
 echo.
-python -m http.server 8000 --bind 127.0.0.1
+python -c "import http.server, socketserver; handler = http.server.SimpleHTTPRequestHandler; handler.extensions_map.update({'.html': 'text/html; charset=utf-8', '.css': 'text/css; charset=utf-8', '.js': 'application/javascript; charset=utf-8'}); httpd = socketserver.TCPServer(('', 8000), handler); print('Server running at http://localhost:8000 with UTF-8 support'); httpd.serve_forever()"
 pause
